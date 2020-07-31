5b9b3f08c58ac013ab7648c26213cb5ea1212137
Ravinder Sokhal <34468640+RavinderSokhal7@users.noreply.github.com>
2020-07-31 15:17:12 +0530

    Update ClimbingStairs.java

--- July_Leetcode_Challenge/ClimbingStairs.java
+++ July_Leetcode_Challenge/ClimbingStairs.java
@@ -57,3 +57,39 @@ class Solution {
         return ans;
     }
 }
+//Solution 3 : time : O ( log n ) : space : O ( 1 ) :: Bilet's method  :: Matrix Multiplicatoin
+class Solution {
+    
+    private void multiply(int [][] ans, int [][] q){
+        int a = 0,b=0,c=0,d=0;
+        a = ans[0][0]*q[0][0] + ans[0][1]*q[1][0];
+        b = ans[0][0]*q[0][1] + ans[0][1]*q[1][1];
+        c = ans[1][0]*q[0][0] + ans[1][1]*q[1][0];
+        d = ans[1][0]*q[0][1] + ans[1][1]*q[1][1];
+        ans[0][0] = a;
+        ans[0][1] = b;
+        ans[1][0] = c;
+        ans[1][1] = d;
+    }
+    
+    private void powM(int[][] ans, int n){
+        if(n==0 || n==1) return;
+        int [][]q = {{1,1}, {1,0}};
+        while(n>0){
+            if((n&1)==1){
+                multiply(ans,q);
+            }
+            multiply(q,q);
+            n/=2;
+        }
+    }
+    
+    public int climbStairs(int n) {
+        if(n==0||n==1) return 1;
+        if(n==2) return 2;
+        int [][] ans = {{1,1} , {1,1}};
+        powM(ans,n-1);
+        return ans[0][0];
+    }
+}
+// solution 4 : using Fibonacci formula
