05fe554f6f811e99150d18edd1f83de83f98408a
Ravinder Sokhal <34468640+RavinderSokhal7@users.noreply.github.com>
2020-09-10 16:24:05 +0530

    day-10 || Bulls and Cows || Freq

---
+++ September_Leetcode_Challenge/BullsAndCows.cpp
@@ -0,0 +1,60 @@
+/*
+Bulls and Cows
+
+You are playing the following Bulls and Cows game with your friend: You write down a number and ask your friend to guess what the number is. Each time your friend makes a guess, you provide a hint that indicates how many digits in said guess match your secret number exactly in both digit and position (called "bulls") and how many digits match the secret number but locate in the wrong position (called "cows"). Your friend will use successive guesses and hints to eventually derive the secret number.
+
+Write a function to return a hint according to the secret number and friend's guess, use A to indicate the bulls and B to indicate the cows. 
+
+Please note that both secret number and friend's guess may contain duplicate digits.
+
+Example 1:
+
+Input: secret = "1807", guess = "7810"
+
+Output: "1A3B"
+
+Explanation: 1 bull and 3 cows. The bull is 8, the cows are 0, 1 and 7.
+
+Example 2:
+
+Input: secret = "1123", guess = "0111"
+
+Output: "1A1B"
+
+Explanation: The 1st 1 in friend's guess is a bull, the 2nd or 3rd 1 is a cow.
+
+Note: You may assume that the secret number and your friend's guess only contain digits, and their lengths are always equal.
+*/
+
+class Solution {
+public:
+    string getHint(string s, string g) {
+        unordered_map<char, int> m;
+        for(char c: s){
+            m[c]++;
+        }
+        int a=0,b=0;
+        for(int i=0;i<g.size();i++){
+            char c = s[i], d = g[i];
+            if(c==d){
+                if(m[c]==0){
+                    b--;m[c]++;
+                }
+                a++;
+                m[c]--;
+            }
+            else{
+                if(m.find(d)!=m.end() && m[d]>0){
+                    m[d]--;
+                    b++;
+                }
+            }
+        }
+        string ans = "";
+        ans +=  to_string(a);
+        ans += "A";
+        ans += to_string(b);
+        ans += "B";
+        return ans;
+    }
+};
